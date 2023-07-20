7171f3e71e32aadd42b020775a9248e842b2fef1
groninge <groninge@gmail.com>
2023-07-20 07:59:16 +0200

    update xboo unwrap

--- scripts/linear_wrap_unwrap_tokens_boo.ts
+++ scripts/linear_wrap_unwrap_tokens_boo.ts
@@ -6,31 +6,32 @@ import { MAX_UINT256 } from '../src/helpers/constants';
 import { BigNumber } from 'ethers';
 import VaultAbi from '../tasks/20210418-vault/artifact/Vault.json';
 import XBOOABI from './abi/xboo.json';
-import IERC20Abi from './abi/IERC20.json';
+import BOOABI from './abi/boo.json';
 import { getAddress } from '@ethersproject/address';
+import { parseUnits } from 'ethers/lib/utils';
 
 const boo = '0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE';
 const xboo = getAddress('0xa48d959AE2E88f1dAA7D5F611E01908106dE7598');
 
-const xbooLinearPoolId = '0x71959b131426fdb7af01de8d7d4149ccaf09f8cc0000000000000000000002e7';
-const xbooLinearPoolAddress = '0x71959b131426fdb7af01de8d7d4149ccaf09f8cc';
+const bbbooLinearPoolId = '0x71959b131426fdb7af01de8d7d4149ccaf09f8cc0000000000000000000002e7';
+const bbbooLinearPoolAddress = '0x71959b131426fdb7af01de8d7d4149ccaf09f8cc';
 
-const sender = '0x8E7Bd7b79d7EA77012a072152794Eb130f8A80f2'; //my wallet
+const sender = process.env.DEPLOYER_ADDRESS || ''; //deployer
 
+const vault = '0x20dd72Ed959b6147912C2e529F0a0C651c33c9ce';
 const remoteVaultAbi = XBOOABI;
 const wrappedTokenAddress = xboo;
-const linearPoolId = xbooLinearPoolId;
-const linearPoolAddress = xbooLinearPoolAddress;
+const linearPoolId = bbbooLinearPoolId;
+const linearPoolAddress = bbbooLinearPoolAddress;
 const mainTokenAddress = boo;
 
 const direction: 'wrap' | 'unwrap' = 'unwrap';
 
 async function loop() {
-  const balancerVault = await ethers.getContractAt(VaultAbi.abi, '0x20dd72Ed959b6147912C2e529F0a0C651c33c9ce');
-
+  const balancerVault = await ethers.getContractAt(VaultAbi.abi, vault);
   const remoteVault = await ethers.getContractAt(remoteVaultAbi, wrappedTokenAddress);
-  const mainToken = await ethers.getContractAt(IERC20Abi, mainTokenAddress);
-  const vaultToken = await ethers.getContractAt(IERC20Abi, wrappedTokenAddress);
+  const mainToken = await ethers.getContractAt(BOOABI, mainTokenAddress);
+  const vaultToken = await ethers.getContractAt(XBOOABI, wrappedTokenAddress);
 
   // 10 USDC
   const mainTokenBalanceUsed = '0';
@@ -70,12 +71,16 @@ async function unwrap(
     return;
   }
 
+  console.log(`Approve swap`);
+  const approve = await mainToken.approve(vault, mainTokenBalance);
+  await approve.wait();
+
   // swap for vault token
   const data: SingleSwap = {
     poolId: linearPoolId,
     kind: 0,
-    assetIn: mainToken,
-    assetOut: vaultToken,
+    assetIn: mainToken.address,
+    assetOut: vaultToken.address,
     amount: mainTokenBalance,
     userData: '0x', //the user data here is not relevant on the swap
   };
@@ -87,9 +92,7 @@ async function unwrap(
     recipient: sender,
   };
 
-  console.log('go swap');
   const swapTransaction = await balancerVault.swap(data, funds, BigNumber.from('0'), MAX_UINT256);
-  console.log('no asdasd');
   await swapTransaction.wait();
   console.log('swap complete ' + i);
 
@@ -162,6 +165,10 @@ async function wrap(
   const swapTransaction = await balancerVault.swap(data, funds, BigNumber.from('0'), MAX_UINT256);
   await swapTransaction.wait();
   console.log('swap complete ' + i);
+}
+
+function formatFixed(mainTokenBalance: BigNumber, arg1: number): any {
+  throw new Error('Function not implemented.');
 }
 /*
 
