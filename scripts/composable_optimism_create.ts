1ffabee10a4b1d69126e819e6cc1839037fc0d95
franz <franzns11@gmail.com>
2023-07-24 16:08:53 +0200

    linear and composable pool changes

--- scripts/composable_optimism_create.ts
+++ scripts/composable_optimism_create.ts
@@ -3,21 +3,10 @@
 import { ethers } from 'hardhat';
 
 import ComposableStablePoolV5Factory from '../tasks/20230711-composable-stable-pool-v5/artifact/ComposableStablePoolFactory.json';
-
-// import ReaperLinearPoolV2Factory from './abi/ReaperLinearPoolV2Factory.json';
-// import ReaperLinearPool from './abi/ReaperLinearPool.json';
-
 import { fp } from '../src/helpers/numbers';
+import { POOL_OWNER_OP, TOKENS } from './constants';
 
-const USDPLUS_LINEAR = '0x88D07558470484c03d3bb44c3ECc36CAfCF43253';
-const DAIPLUS_LINEAR = '0xb5ad7d6d6F92a77F47f98C28C84893FBccc94809';
-const ERN = '0xc5b001dc33727f8f26880b184090d3e252470d45';
-const bbrfUSDC = '0xDc2007D9e9A33f50630F26069FAab69c25f7758C';
-const bbrfUSDT = '0xBEF1ccAaDA458a570C37B11A8872988bA1E4FDb9';
-const bbrfusd = '0x64cee2338369aa9b36fc756ea231eb9bc242926f';
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 const COMPOSABLESTABLE_POOL_V5_FACTORY_ADDRESS = '0x043A2daD730d585C44FB79D2614F295D2d625412';
-const POOL_OWNER = '0xd9e2889AC8C6fFF8e94c7c1bEEAde1352dF1A513';
 
 async function create() {
   const factory = await ethers.getContractAt(
@@ -28,32 +17,13 @@ async function create() {
   // BEGIN ====== variables ====== BEGIN
 
   const poolName = 'Dollar Dollar Bills';
-  const poolSymbol = 'bb-doldol';
+  const poolSymbol = 'bb-dolbil';
   const amp = 40;
   const feeExemption = false;
-  const swapFee = fp(0.0005);
-  const random = '0xc4084939b87f183e144bbf3fdbcbbd0c356f0813344f298f8de1404f5e6d20e2'; // https://www.browserling.com/tools/random-hex
+  const swapFee = fp(0.0001);
+  const random = '0x5342255c78acbda934a14fbf3c37085a80a36ba6651c6fa0c5d5570b5340330d'; // https://www.browserling.com/tools/random-hex
 
-  const tokens = [
-    {
-      name: 'ERN',
-      address: ERN,
-      rateProvider: ZERO_ADDRESS,
-      cache: 30,
-    },
-    {
-      name: 'bbrfUSDC',
-      address: bbrfUSDC,
-      rateProvider: bbrfUSDC,
-      cache: 30,
-    },
-    {
-      name: 'bbrfUSDT',
-      address: bbrfUSDT,
-      rateProvider: bbrfUSDT,
-      cache: 30,
-    },
-  ];
+  const tokens = [TOKENS['OPTIMISM'].ERN, TOKENS['OPTIMISM'].BBRFUSDC, TOKENS['OPTIMISM'].BBRFUSDT];
 
   // END ====== variables ====== END
 
@@ -72,7 +42,7 @@ async function create() {
     sortedTokens.map((token) => token.cache), // cache
     feeExemption, //fee exemption
     swapFee, //swap fee
-    POOL_OWNER,
+    POOL_OWNER_OP,
     random //random string
   );
 
--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,9 +1,9 @@
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-export const TOKENS: Record<
-  string,
-  Record<string, { address: string; decimals: number; rateProvider: string; cache: number }>
-> = {
+export const POOL_OWNER_OP = '0xd9e2889AC8C6fFF8e94c7c1bEEAde1352dF1A513';
+export const VAULT_ADDRESS_OP = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
+
+export const TOKENS = {
   ['FANTOM']: {
     WFTM: {
       address: '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83',
@@ -13,12 +13,6 @@ export const TOKENS: Record<
     },
   },
   ['OPTIMISM']: {
-    ETH: {
-      address: 'AddressZero',
-      decimals: 18,
-      rateProvider: ZERO_ADDRESS,
-      cache: 30,
-    },
     USDC: {
       address: '0x7F5c764cBc14f9669B88837ca1490cCa17c31607',
       decimals: 6,
@@ -38,45 +32,51 @@ export const TOKENS: Record<
       cache: 30,
     },
     BBRFUSDC: {
-      address: '0xDc2007D9e9A33f50630F26069FAab69c25f7758C',
+      address: '0xf970659221bb9d01b615321b63a26e857ffc030b',
       decimals: 18,
-      rateProvider: '0xDc2007D9e9A33f50630F26069FAab69c25f7758C',
+      rateProvider: '0xf970659221bb9d01b615321b63a26e857ffc030b',
       cache: 30,
     },
     BBRFWSTETH: {
-      address: '0xB85245929dc65B5EDdb56c4b4E84b20BcE69Db35',
+      address: '0x48ace81c09382bfc08ed102e7eadd37e3b049752',
       decimals: 18,
-      rateProvider: '0xB85245929dc65B5EDdb56c4b4E84b20BcE69Db35',
+      rateProvider: '0x48ace81c09382bfc08ed102e7eadd37e3b049752',
       cache: 30,
     },
     BBRFWBTC: {
-      address: '0xd32F78F5aE235269c6d2cABBD26A57fF9FD62967',
+      address: '0x8025586ac5fb265a23b9492e7414beccc2059ec3',
       decimals: 18,
-      rateProvider: '0xd32F78F5aE235269c6d2cABBD26A57fF9FD62967',
+      rateProvider: '0x8025586ac5fb265a23b9492e7414beccc2059ec3',
       cache: 30,
     },
     BBRFUSDT: {
-      address: '0xBEF1ccAaDA458a570C37B11A8872988bA1E4FDb9',
+      address: '0x20715545c15c76461861cb0d6ba96929766d05a5',
       decimals: 18,
-      rateProvider: '0xBEF1ccAaDA458a570C37B11A8872988bA1E4FDb9',
+      rateProvider: '0x20715545c15c76461861cb0d6ba96929766d05a5',
       cache: 30,
     },
     BBRFOP: {
-      address: '0x55b1F937B1335Be355C82e207FD437182c986Ba1',
+      address: '0x3e9cbffd270ae67abb09d28988e7e785498c7373',
       decimals: 18,
-      rateProvider: '0x55b1F937B1335Be355C82e207FD437182c986Ba1',
+      rateProvider: '0x3e9cbffd270ae67abb09d28988e7e785498c7373',
       cache: 30,
     },
     BBRFWETH: {
-      address: '0x8003eec4aDD35C6D23eB1Ef61b4fA6bbBB23a41a',
+      address: '0x2e2b8b82123789d895fd79913f6dfa51f5b5a0e6',
       decimals: 18,
-      rateProvider: '0x8003eec4aDD35C6D23eB1Ef61b4fA6bbBB23a41a',
+      rateProvider: '0x2e2b8b82123789d895fd79913f6dfa51f5b5a0e6',
       cache: 30,
     },
     BBRFDAI: {
-      address: '0x8fE33d737484CA194dedF64AaFa8485327fC5372',
+      address: '0xa5d4802b4ce6b745b0c9e1b4a79c093d197869c8',
       decimals: 18,
-      rateProvider: '0x8fE33d737484CA194dedF64AaFa8485327fC5372',
+      rateProvider: '0xa5d4802b4ce6b745b0c9e1b4a79c093d197869c8',
+      cache: 30,
+    },
+    ERN: {
+      address: '0xc5b001dc33727f8f26880b184090d3e252470d45',
+      decimals: 18,
+      rateProvider: ZERO_ADDRESS,
       cache: 30,
     },
   },
