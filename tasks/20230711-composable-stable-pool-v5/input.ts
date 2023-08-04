93d700fb506f3b52443e54fba4d699ec54b2e61c
groninge <groninge@gmail.com>
2023-08-04 11:17:41 +0200

    update composable stable input

--- tasks/20230711-composable-stable-pool-v5/input.ts
+++ tasks/20230711-composable-stable-pool-v5/input.ts
@@ -10,7 +10,11 @@ export type ComposableStablePoolDeployment = {
 };
 
 const Vault = new Task('20210418-vault', TaskMode.READ_ONLY);
-const ProtocolFeePercentagesProvider = new Task('20220725-protocol-fee-percentages-provider', TaskMode.READ_ONLY);
+//const ProtocolFeePercentagesProvider = new Task('20220725-protocol-fee-percentages-provider', TaskMode.READ_ONLY);
+const PoolSpecificProtocolFeePercentagesProvider = new Task(
+  '20220725-protocol-fee-percentages-provider',
+  TaskMode.READ_ONLY
+);
 const WETH = new Task('00000000-tokens', TaskMode.READ_ONLY);
 const BAL = new Task('00000000-tokens', TaskMode.READ_ONLY);
 
@@ -18,7 +22,7 @@ const BaseVersion = { version: 5, deployment: '20230711-composable-stable-pool-v
 
 export default {
   Vault,
-  ProtocolFeePercentagesProvider,
+  ProtocolFeePercentagesProvider: PoolSpecificProtocolFeePercentagesProvider,
   WETH,
   BAL,
   FactoryVersion: JSON.stringify({ name: 'ComposableStablePoolFactory', ...BaseVersion }),
